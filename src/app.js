c3af2929f35d7d1ee1c3930a20e53bb09d78bf4d
Mykyta  Romanishyn <mromanishyn@medenvoyglobal.com>
2024-12-19 16:17:53 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 // write code here
+const fs = require('fs');
+const path = require('path');
+
+function moveFile() {
+  const [from, to] = process.argv.slice(2);
+
+  const dest =
+    fs.existsSync(to) && fs.statSync(to).isDirectory()
+      ? path.join(to, path.basename(from))
+      : to;
+
+  try {
+    fs.renameSync(from, dest);
+    // eslint-disable-next-line no-console
+    console.log(`File moved to ${dest}`);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(`Failed to move file: ${err.message}`);
+  }
+}
+
+moveFile();
